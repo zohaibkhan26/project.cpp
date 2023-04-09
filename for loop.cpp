c5e386ac09cd4777e425def0c53a5e83969c1040
zohaibkhan26 <130275355+zohaibkhan26@users.noreply.github.com>
2023-04-09 13:08:29 +0500

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//for loop 
+//programm 1
+/*for(  int i=0; i<=;i++){
+	cout<<"hi"<<endl;  
+}
+//programm 2
+for(int i=0; i<10; i++){
+	cout<<i<<endl;
+	
+}
+//programm3
+for (int i=0; i<10; i++)
+{
+	cout<<(i+1)*100<<endl;
+	//excution will occur infinite time 
+}
+//programm4
+
+for( int i=0; i<10; i=i+1.5){ 
+	cout<<i;
+}
+//note:as weknow that int store only integer number not desimal number therfore programm willexecute 10 time(ingoring .5 value ) 
+
+//programm5
+
+for(int i=0; i<10; i=i+0.5){ 
+	cout<<i;
+}
+//note:as weknow that int store only integer number not desimal number therfore programm willexecute 10 time(ingoring .5 value )  
+// and programm will execute infinite time
+
+
+//programm 6
+int i =0; 
+for( ; i<10; i++){
+	cout<<i;
+}
+
+//programm 7
+int i =0; 
+for( ;i<10 ; ){
+	cout<<i++;
+}*/
+//programm
+ int i=0;
+for( ; ; ){
+	if(i<10){
+	
+	cout<<i++;
+	}
+	else
+	continue;
+}
+
+}
+
